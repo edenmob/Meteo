27569aeba82447b5c4c05d6237f7814a303eb401
Brahim AIT ELHAJ <tech@edenmob.com>
2015-05-25 22:31:18 +0200

    ex2_adding_views : adding views and fragment

--- app/src/main/java/com/example/training/meteo/MainActivity.java
+++ app/src/main/java/com/example/training/meteo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.training.meteo;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -33,5 +37,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
     }
 }
