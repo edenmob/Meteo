b07bf1553da9f38a7ed0b324c43c6ffdfa4cee0d
Brahim AIT ELHAJ <tech@edenmob.com>
2015-06-22 21:04:19 +0200

    Ex34 : Detail view with loader

--- app/src/main/java/com/example/training/meteo/DetailFragment.java
+++ app/src/main/java/com/example/training/meteo/DetailFragment.java
@@ -1,24 +1,51 @@
 package com.example.training.meteo;
 
+
 import android.content.Intent;
+
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.training.meteo.data.WeatherContract;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DetailFragment extends Fragment {
+public class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     public static final String TAG = DetailFragment.class.getSimpleName();
 
     public static final String DETAIL_URI  = "URI";
+    private static final int DETAIL_LOADER = 0;
+
+    private static final String[] FORECAST_COLUMNS = {
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+    };
+
+    // these constants correspond to the projection defined above, and must change if the
+    // projection changes
+    private static final int COL_WEATHER_ID = 0;
+    private static final int COL_WEATHER_DATE = 1;
+    private static final int COL_WEATHER_DESC = 2;
+    private static final int COL_WEATHER_MAX_TEMP = 3;
+    private static final int COL_WEATHER_MIN_TEMP = 4;
+
+    private String mForecast;
 
     public DetailFragment() {
     }
@@ -28,16 +55,75 @@ public class DetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(DETAIL_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Log.v(TAG, "In onCreateLoader");
+
         Bundle arguments = getArguments();
 
+        Uri detailUri = null;
+
         if (arguments != null) {
-            Uri detailUri = arguments.getParcelable(DETAIL_URI);
+            detailUri = arguments.getParcelable(DETAIL_URI);
             //String forecastData = arguments.getString(Intent.EXTRA_TEXT);
             Log.d(TAG,arguments.toString());
             /*((TextView) rootView.findViewById(R.id.detail_text))
                     .setText(forecastData);*/
         }
 
-        return rootView;
+
+        if(detailUri == null){
+            return null;
+        }
+
+
+        // Now create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed.
+        return new CursorLoader(
+                getActivity(),
+                detailUri,
+                FORECAST_COLUMNS,
+                null,
+                null,
+                null
+        );
     }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.v(TAG, "In onLoadFinished");
+        if (!data.moveToFirst()) { return; }
+
+        String dateString = Utility.formatDate(
+                data.getLong(COL_WEATHER_DATE));
+
+        String weatherDescription =
+                data.getString(COL_WEATHER_DESC);
+
+        boolean isMetric = Utility.isMetric(getActivity());
+
+        String high = Utility.formatTemperature(
+                data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+
+        String low = Utility.formatTemperature(
+                data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+
+        mForecast = String.format("%s - %s - %s/%s", dateString, weatherDescription, high, low);
+
+        TextView detailTextView = (TextView)getView().findViewById(R.id.detail_text);
+        detailTextView.setText(mForecast);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) { }
 }
